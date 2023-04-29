7cbdf8b4f204f9caa29c9cf8304d43e337b4df21
Alex Bernt <alexbernt100@gmail.com>
2023-04-28 19:31:17 -0500

    Delete unused code

--- client/components/AddNewRun.js
+++ client/components/AddNewRun.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { Button } from "@mui/material";
 import { Container } from "@mui/material";
 import { Typography } from "@mui/material";
